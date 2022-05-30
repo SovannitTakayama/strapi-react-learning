972f750b9ca37011f33293e53de86104b929f59f
sovannit <sovannit.o@codium.co>
2022-05-30 15:30:44 +0700

    Config for deploy backend

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: cd strapi_backend
+web: yarn install
+web: yarn start
--- strapi_backend/config/database.js
+++ strapi_backend/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'postgres'),
+      host: env('DATABASE_HOST', 'ec2-54-172-175-251.compute-1.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'my_db'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'ddaehuiqu163u9'),
+      user: env('DATABASE_USERNAME', 'fysaorrgvlhkwl'),
+      password: env('DATABASE_PASSWORD', 'a100e7d741fa1ef5fe4c0073d71009c65715931a71e24be3a378041952add7a5'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
